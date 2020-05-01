782f1b11da9f4758948b11796f363432ba2f125a
Dexter Freeman <vse.pisma.tut@gmail.com>
2020-05-01 17:43:42 +0300

    Add Chart.js and create chart visualisation for data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     async function getData() {
+        // create veraibles for data
+        const years = [];
+        const temps = [];
         // get data from local file
         const response = await fetch('ZonAnn.Ts+dSST.csv');
         // grab text from the response
@@ -12,11 +15,48 @@ document.addEventListener('DOMContentLoaded', () => {
             // grab only year and Glob temp
             const columns = row.split(',');
             const year = columns[0];
+            years.push(year);
             const temp = columns[1];
-            console.log(year, temp);
+            temps.push(parseFloat(temp) + 14);
         })
-
+        // return object with years and temps
+        return { years, temps };
     }
-    
-    getData();
+
+
+
+    async function chartIt() {
+        // get data
+        const data = await getData();
+
+        var ctx = document.getElementById('chart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: data.years,
+                datasets: [{
+                    label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in C°',
+                    data: data.temps,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            // Include a ° sign in the ticks
+                            callback: function(value, index, values) {
+                                return value + '°';
+                            }
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+    chartIt();
 });
